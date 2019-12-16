1b997827041ffe05d103856c71eca1287a32e273
Vytautas <vytautasragaisis07@gmail.com>
2019-12-16 23:56:44 +0200

    initial commit

--- js/main.js
+++ js/main.js
@@ -36,20 +36,23 @@ window.onload = function() {
                 let valandos = new Date(data['forecastTimestamps'][i]['forecastTimeUtc']);
                 let valanda = valandos.getHours() + ':00';
                 let valandele = document.createElement('p');
+                valandele.className = 'valandosp'+[i];
                 valandele.textContent = valanda;
                 blokas.appendChild(valandele);
 
                 /* Ikonėlė */
 
                 let image = document.createElement('div');
+                image.className = 'ikonele'+[i];
                 let imageValue = data['forecastTimestamps'][i]['conditionCode'];
-                image.innerHTML = `<img src="icons/${imageValue}.png"/>`;
+                image.innerHTML = `<img id="icon" src="icons/${imageValue}.png"/>`;
                 blokas.appendChild(image);
 
                 /* Oro Temperatūra */
 
                 let tempValue = data['forecastTimestamps'][i]['airTemperature'];
                 let temperatura = document.createElement('p');
+                temperatura.className = 'temperaturap'+[i];
                 temperatura.textContent = tempValue + '°C';
                 blokas.appendChild(temperatura);
 
@@ -57,6 +60,7 @@ window.onload = function() {
 
                 let kritValue = data['forecastTimestamps'][i]['totalPrecipitation'];
                 let krituliai = document.createElement('p');
+                krituliai.className = 'krituliaip'+[i];
                 krituliai.textContent = kritValue + '%';
                 blokas.appendChild(krituliai);
 
@@ -64,10 +68,9 @@ window.onload = function() {
 
                 let windValue = data['forecastTimestamps'][i]['windSpeed'];
                 let vejas = document.createElement('p');
+                vejas.className = 'vejop'+[i];
                 vejas.textContent = windValue;
                 blokas.appendChild(vejas);
-
-                console.log(data['forecastTimestamps'][i]['conditionCode']);
             }
         });
 };
@@ -85,51 +88,43 @@ button.addEventListener('click', function() {
                 let nameValue = data['place']['name'];
                 let name = document.querySelector('.miestas');
                 name.innerHTML = nameValue;
-                document.getElementsByClassName('miestas').innerHTML = nameValue;
-
-                /* Sukuriamas valandai atskiras blokas */
-
-                let blokas = document.createElement('div');
-                blokas.className = 'blokelis';
-                valandu.appendChild(blokas);
 
                 /* Valandos Laikas */
 
                 let valandos = new Date(data['forecastTimestamps'][i]['forecastTimeUtc']);
                 let valanda = valandos.getHours() + ':00';
-                let valandele = document.createElement('p');
+                let valandele = document.querySelector('.valandosp'+[i]);
+                console.log(valanda);
                 valandele.textContent = valanda;
-                blokas.appendChild(valandele);
 
                 /* Ikonėlė */
 
-                let image = document.createElement('div');
+                let image = document.querySelector('.ikonele'+[i]);
                 let imageValue = data['forecastTimestamps'][i]['conditionCode'];
-                image.innerHTML = `<img src="icons/${imageValue}.png"/>`;
-                blokas.appendChild(image);
+                image.innerHTML = `<img id="icon" src="icons/${imageValue}.png"/>`;
+
 
                 /* Oro Temperatūra */
 
                 let tempValue = data['forecastTimestamps'][i]['airTemperature'];
-                let temperatura = document.createElement('p');
-                temperatura.textContent = tempValue + '°C';
-                blokas.appendChild(temperatura);
+                let temperatura = document.querySelector('.temperaturap'+[i]);
+                temperatura.innerHTML= tempValue + '°C';
+
 
                 /* Krituliai */
 
                 let kritValue = data['forecastTimestamps'][i]['totalPrecipitation'];
-                let krituliai = document.createElement('p');
-                krituliai.textContent = kritValue + '%';
-                blokas.appendChild(krituliai);
+                let krituliai = document.querySelector('.krituliaip'+[i]);
+                krituliai.innerHTML = kritValue + '%';
 
                 /* Vėjo greitis */
 
                 let windValue = data['forecastTimestamps'][i]['windSpeed'];
-                let vejas = document.createElement('p');
-                vejas.textContent = windValue;
-                blokas.appendChild(vejas);
+                let vejas = document.querySelector('.vejop'+[i]);
+                vejas.innerHTML = windValue;
 
-                console.log(data['forecastTimestamps'][i]['conditionCode']);
+
+                console.log(data['forecastTimestamps'][i]['forecastTimeUtc']);
             }
         });
 });
